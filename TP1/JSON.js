316e57456291a2c8c9beb47c8a56c6d4c03325c6
Federico Guigou <fefogs@gmail.com>
2024-04-12 16:06:46 -0300

    funciones agregadas para comparar generos

--- TP1/JSON.js
+++ TP1/JSON.js
@@ -20,8 +20,7 @@ const personaje =
      ],
      "url": "https://rickandmortyapi.com/api/character/140",
      "created": "2017-12-27T18:47:44.566Z"
-    }
-    
+    } 
 /*console.log(Personaje);*/
 console.log(personaje.name);
 console.log(personaje.location.name);
@@ -38,8 +37,64 @@ const jugador ={
 console.log(jugador.clubes[0]);
 console.log("El Personaje ",personaje.name, " está ", personaje.status)
 
-function estado(){
-    alert("El Personaje "+personaje.name+ " está "+ personaje.status)
+/*document.getElementById("result").innerHTML= "El Personaje "+personaje.name+ " está "+ personaje.status;*/
+/* 11/04/2024:  */
+
+function nombre(){
+    var valor = document.getElementById("valor").value;
+    console.log("Personaje: "+ valor);
+    fetch("https://rickandmortyapi.com/api/character/"+valor)
+    .then(response => response.json()
+        .then(data => {
+            console.log(data.name);
+        }));
 };
 
-document.getElementById("result").innerHTML= "El Personaje "+personaje.name+ " está "+ personaje.status;
+/* 12/04/2024:  */
+var genero1=" ", genero2=" ", numPersonaje= Number;
+
+function buscar_genero1(){
+    let imagen = document.getElementById("p1");
+    numPersonaje = document.getElementById("Numero_Personaje1").value;
+    fetch("https://rickandmortyapi.com/api/character/"+numPersonaje)
+        .then (response => response.json()
+            .then (data => {
+                console.log("PERSONAJE 1:  Num: "+numPersonaje + " - Nombre: "+data.name + " - Genero: "+data.gender);
+                genero1= data.gender;
+                imagen.src= data.image;
+                imagen.width="100";
+                imagen.height="100";
+            }))
+}/* BUSCAR cómo hacer que se use una sola función para ambos input */
+function buscar_genero2(){
+    let imagen = document.getElementById("p2");
+    numPersonaje = document.getElementById("Numero_Personaje2").value;
+    fetch("https://rickandmortyapi.com/api/character/"+numPersonaje)
+        .then (response => response.json()
+            .then (data => {
+                console.log("PERSONAJE 2:  Num: "+numPersonaje + " - Nombre: "+data.name + " - Genero: "+data.gender);
+                genero2= data.gender;
+                imagen.src= data.image;
+                imagen.width="100";
+                imagen.height="100";         
+            }))
+}
+
+function comparar_generos(){
+    let imagen = document.getElementById("resultado");
+    console.log("comparando...");
+    if (genero1 == genero2){
+        imagen.src= "https://segboardshoppen.dk/wp-content/uploads/2019/02/checkmark-for-verification.svg"
+        imagen.width="100";
+        imagen.height="100";
+        imagen.alt="tienen el mismo genero";
+        }
+        else {
+            imagen.src= "https://cdn.pixabay.com/photo/2012/04/13/00/22/red-31226_960_720.png";
+            imagen.width="100";
+            imagen.height="100";
+            imagen.alt="NO tienen el mismo genero";
+        }
+    
+}
+
