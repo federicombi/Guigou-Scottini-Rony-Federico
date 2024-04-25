b05f6e34836e00d9cc472c912922fc3a242d9421
Federico Guigou <fefogs@gmail.com>
2024-04-25 16:34:20 -0300

    mapa y funciones corregidas

--- TP3/fn.js
+++ TP3/fn.js
@@ -1,24 +1,20 @@
-/*
-function obtenerUser(){
-    fetch("https://randomuser.me/api/")
-        .then((results)=>{
-            return results.json();
-        })
-            .then ((data)=>{
-                console.log(data.results);
-            });
-};
-*/
+
+var nombre, apellido = " ";
+let latitud, longitud = 0;
+let map= L.map("map").setView([-33.0079, -58.5224], 12);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+/*var marker = L.marker([51.5, -10.09]);*/
 console.log("ready");
-let latitud="";
-let longitud="";
 
 function probar(){
     console.log("funca")
 }
 
-function obtenerUser(){
-    const urlRandomUser="https://randomuser.me/api/";
+function generateUser(){
+    const urlRandomUser="https://randomuser.me/api/?nat=ES";
     fetch(urlRandomUser)
         .then(response => response.json())
             .then(data => {
@@ -26,9 +22,16 @@ function obtenerUser(){
                 latitud = data.results[0].location.coordinates.latitude;
                 longitud = data.results[0].location.coordinates.longitude;
                 console.log(data.results[0].location.coordinates.longitude);
+                nombre = data.results[0].name.first;
+                apellido = data.results[0].name.last;
+                document.getElementById("username").innerHTML = nombre+" "+apellido;
             });
 }
 
 function createMap(){
-    let map= L.map("map").setView([51.505, 51.09], 13);
+    /*map.flyTo([longitud, latitud], 13)*/
+    map.panTo(new L.LatLng(latitud,longitud));
+    marker = L.marker([latitud,longitud]).addTo(map);
+    marker.bindPopup("<b>"+nombre+" "+apellido+"</b>").openPopup();
 }
+
